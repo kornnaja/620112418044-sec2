a5abdbba41638804369ee76e073e208d4bf861b8
kornnaja <76421907+kornnaja@users.noreply.github.com>
2022-12-01 11:49:24 +0700

    lab4

--- lab4/php/index.php
+++ lab4/php/index.php
@@ -1,12 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>fsdffs</h1>
-</body>
-</html>
+<?php
+#phpinfo();
+
+
+$num = 10;
+$name = "kkkk";
+
+echo "GET :" . $_GET['name'];
+echo "POST :" . $_POST['pass'];
+
+
+?>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>name</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                1
+            </td>
+            <td>
+                <?php echo $name ?>
+            </td>
+        </tr>
+        <?php
+        for ($i = 2; $i < 5; $i++) {
+        ?>
+            <tr>
+                <td>
+                    <?php echo $i  ?>
+                </td>
+                <td>
+                    <? echo $name ?>
+                </td>
+            </tr>
+        <?php  } ?>
+    </tbody>
+</table>
